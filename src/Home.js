9f1082b8924baea0da064f21d86c0e85a799c87c
unknown <rufus.lansisalmi@gmail.com>
2022-03-29 11:28:46 +0300

    second commit

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,42 @@
+import React from "react";
 import { useState } from "react";
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 
+
+function valuetext(value) {
+    return `${value}°C`;
+  }
+  
+
+ 
 
 const Home = () => {
+
+    const [value, setValue] = React.useState([10, 18]);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+        
+      };
+      const marks = [
+        {
+          value: 6,
+          label: '6',
+        },
+        {
+          value: 12,
+          label: '12',
+        },
+        {
+          value: 18,
+          label: '18',
+        },
+        {
+          value: 24,
+          label: '24',
+        },
+      ];
     
         const [days, setdays] = useState([
             {title: 'Mon', id: 1},
@@ -15,11 +50,31 @@ const Home = () => {
         ]);
       
     return ( 
+        
         <div className="home">
-           {days.map((day) => (
+            
+            {days.map((day) => (
                <div className="day-preview" key={day.id} >
                    <h2>{day.title}</h2>
                    <input type="checkbox" />
+                    <Box sx={{width: 600}}>
+            <Slider 
+            getAriaLabel={() => 'Temperature range'}
+             value={value}
+             onChange={handleChange}
+             valueLabelDisplay="auto"
+             getAriaValueText={valuetext}
+             marks={marks}
+             min={6}
+             max={24}
+             
+            />
+        </Box>
+                   
+                   
+                   
+                 
+                   
                    
                    
                </div>
